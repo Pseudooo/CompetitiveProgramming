e9e05908e27832657ac1a61ce26f2eb3bf3d0ae7
Pseudo <mitchell.wright877@gmail.com>
2018-11-09 11:19:22 +0000

    Made Progress

--- UKIEPC2015/ProblemC.py
+++ UKIEPC2015/ProblemC.py
@@ -11,3 +11,58 @@ in the instance of a tie however they should be ordered in terms of which
 was the most frequent.
 
 '''
+
+# ====== COLLECT INPUTS
+
+# n quantity represents the amount of messages
+n = int(input())
+
+# A list of n messages that have been sent
+Messages = []
+
+for i in range(n):
+    Messages.append(input().split())
+
+# ====== PROCESS
+
+# MsgFreq dict contains frequency of occurence of each message
+MsgFreq = {}
+
+UserMessages = {}
+
+for msg in Messages:
+    
+    # First build the frequency dictionary
+    for wrd in msg[1:]:
+        if wrd in MsgFreq.keys():
+            MsgFreq[wrd]+=1
+        else:
+            MsgFreq[wrd] = 1
+    
+    user = msg[0]
+    MsgSet = set(msg[1:])
+    
+    if user in UserMessages.keys():
+        UserMessages[user] = set.union(UserMessages[user], MsgSet)
+    else:
+        UserMessages[user] = MsgSet
+    
+# Now we look at the intersection of messages between sets
+SharedMessages = list(set.intersection(*[UserMessages[a] for a in UserMessages.keys()]))
+
+# Check if the shared messages list is empty, if so edge case ALL CLEAR
+if SharedMessages == []:
+    print("ALL CLEAR") # No shared messages between users
+else:
+    # Not empty
+    
+    # Now sort shared messages by message frequency
+    SharedMessages.sort(key=lambda a : MsgFreq[a], reverse=True)
+
+    # TODO Find blocks of same frequency and sort alphabetically
+    
+
+    
+    
+    
+    
