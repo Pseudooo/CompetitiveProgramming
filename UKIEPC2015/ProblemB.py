687e716434b52525777389c6d5d545c19ef0bcaf
Pseudo <mitchell.wright877@gmail.com>
2018-11-10 16:43:35 +0000

    Removed block comment describing problem/solution as it's in readme

--- UKIEPC2015/ProblemB.py
+++ UKIEPC2015/ProblemB.py
@@ -1,26 +1,3 @@
-'''
-
-Mountain Biking:
-
-This problem features finding the speed required of a cyclist to reach the
-top of the mountain without walking. Therefore the speed required of the 
-cyclist at the start of each segment must be calculated in a way that allows
-for them to reach the top.
-
-Solution:
-
-In order to make things a bit easier to calculate we're going to reverse things
-and imagine that they start at the top of the hill with speed 0 and calculate
-the velocity of the cyclist as they reach the end of each segment. This can be
-done much easier using v^2=u^2+2as (SUVAT) equation. So we start by collecting
-all of the required inputs, we then reverse the order of the list of segments
-inputted and iterate through the new list. At each iteration we can calculate
-the final velocity of the cyclist given a=g*cos(theta) and s is given for the
-segment. u is just our previous velocity which is initially 0. At the end we 
-simply reverse the order of v again and print it.
-
-'''
-
 import math as m
 
 # Collect the n number of segments and gravitational constant g.
--- UKIEPC2015/ProblemC.py
+++ UKIEPC2015/ProblemC.py
@@ -1,17 +1,3 @@
-'''
-Conversation Log:
-
-This problem features an integer input of n which includes the number of
-messages that have been sent in total, each message is sent by a user and
-the goal of this problem is to identify all of the messages that are shared
-between all of the n messages.
-
-All of these shared messages should then be outputted in alphabetical order,
-in the instance of a tie however they should be ordered in terms of which
-was the most frequent.
-
-'''
-
 # ====== COLLECT INPUTS
 
 # n quantity represents the amount of messages
